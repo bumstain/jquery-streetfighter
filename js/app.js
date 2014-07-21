1f334047403e278584af4c24c87f7a601c1e8688
Ben Stein <bumstain@gmail.com>
2014-07-21 22:18:30 +0100

    Fixed errors and updated layouts and sound

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+  //show Ryu's still state
+  $('.ryu-still').show();
+  $('.ryu-throwing').hide();
+  $('.ryu-ready').hide();
+  $('.ryu-cool').hide();
+  playOpen();
+
   //show Ryu's ready state
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
@@ -18,6 +25,7 @@ $(document).ready(function() {
     playHadouken();    
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
+    $('.ryu-still').hide();
     $('.hadouken').finish().show()
     .animate(
       {'left': '860px'},
@@ -33,9 +41,9 @@ $(document).ready(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
-});
 
-$(this).keydown(function(event) { 
+
+  $(this).keydown(function(event) { 
     if(event.which == 88) {   
       event.preventDefault();
       $('.ryu-still').hide();
@@ -53,8 +61,24 @@ $(this).keydown(function(event) {
     $('.ryu-still').show();
   });
   
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
-}
+  function playHadouken () {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+  }
+
+  function playOpen () {
+    $('#open-sound')[0].volume = 0.5;
+    $('#open-sound')[0].load();
+    $('#open-sound')[0].play();
+  }
+
+  function playKarateKid () {
+    $('#KarateKid-sound')[0].volume = 0.5;
+    $('#KarateKid-sound')[0].load();
+    $('#KarateKid-sound')[0].play();
+  }
+
+});
+
+
